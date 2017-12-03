d3e7a59cd570171b2d294b82516c4f857ccf4b2f
lycoris0731 <aizu.s1230022@gmail.com>
2017-12-03 11:03:02 +0900

    implement commit parser

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"compress/zlib"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/k0kubun/pp"
 )
@@ -19,45 +21,78 @@ const (
 type Object struct {
 	Type string
 	Size int
-	Body []byte
+	Body *Commit
 }
 
-func parseType(r io.Reader) ([]byte, error) {
-	buf := make([]byte, typeMaxLen)
-	_, err := r.Read(buf)
-	if err != nil {
-		return nil, err
-	}
-	for i := range buf {
-		if buf[i] == ' ' {
-			return buf[:i], nil
-		}
-	}
-
-	return buf, nil
+type Commit struct {
+	Tree      string
+	Parent    []string
+	Author    *Author
+	Committer *Committer
 }
 
-func parseSize(r io.Reader) ([]byte, error) {
-	buf := bufio.NewReader(r)
+type person struct {
+	Username  string
+	Email     string
+	Timestamp string
+}
 
-	buf.ReadByte() // skip " "
+type Author person
+type Committer person
 
-	b, err := buf.ReadSlice(0x00)
+func parseType(buf *bytes.Buffer) (int, []byte, error) {
+	return bufio.ScanWords(buf.Bytes(), false)
+}
+
+func parseSize(buf *bytes.Buffer) ([]byte, error) {
+	b, err := buf.ReadBytes(0x00)
 	if err != nil {
 		return nil, err
 	}
 	return b[:len(b)-1], nil
 }
 
-func Parse(r io.Reader) (*Object, error) {
+func parseCommit(s *bufio.Scanner) (*Commit, error) {
+	var commit Commit
+	for s.Scan() {
+		line := s.Text()
+		sp := strings.Split(line, " ")
+		switch sp[0] {
+		case "tree":
+			commit.Tree = sp[1]
+		case "parent":
+			commit.Parent = append(commit.Parent, sp[1])
+		case "author":
+		case "committer":
+			username, email, timestamp := sp[1], sp[2], strings.Join(sp[3:], " ")
+			if sp[0] == "author" {
+				commit.Author = &Author{
+					Username:  username,
+					Email:     email,
+					Timestamp: timestamp,
+				}
+			} else {
+				commit.Committer = &Committer{
+					Username:  username,
+					Email:     email,
+					Timestamp: timestamp,
+				}
+			}
+		}
+	}
+	return &commit, nil
+}
+
+func Parse(buf *bytes.Buffer) (*Object, error) {
 	var obj Object
-	b, err := parseType(r)
+	n, b, err := parseType(buf)
+	buf.Next(n)
 	if err != nil {
 		return nil, err
 	}
 	obj.Type = string(b)
 
-	b, err = parseSize(r)
+	b, err = parseSize(buf)
 	if err != nil {
 		return nil, err
 	}
@@ -65,6 +100,14 @@ func Parse(r io.Reader) (*Object, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	s := bufio.NewScanner(buf)
+	s.Scan() // skip header
+	commit, err := parseCommit(s)
+	if err != nil {
+		return nil, err
+	}
+	obj.Body = commit
 
 	return &obj, nil
 }
@@ -75,7 +118,6 @@ func main() {
 	}
 
 	p := filepath.Join(".git", "objects", os.Args[1][:2], os.Args[1][2:])
-	fmt.Println(p)
 	f, err := os.Open(p)
 	if err != nil {
 		panic(err)
@@ -88,7 +130,15 @@ func main() {
 	}
 	defer r.Close()
 
-	obj, err := Parse(r)
+	buf := &bytes.Buffer{}
+	_, err = io.Copy(buf, r)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(buf.String())
+
+	obj, err := Parse(buf)
 	if err != nil {
 		panic(err)
 	}
